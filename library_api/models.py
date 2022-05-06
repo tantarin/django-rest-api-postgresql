064796b4782c1a02daa5d81ddb265ee5a30c0050
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2022-05-07 00:31:01 +0300

    change models

--- library_api/models.py
+++ library_api/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
