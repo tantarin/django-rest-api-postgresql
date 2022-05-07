a6303068fe5dfa68220fd1b0e8dcc10492b67cb4
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2022-05-07 19:55:27 +0300

    edit some code

--- library_api/views.py
+++ library_api/views.py
@@ -1,9 +1,9 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render
+from rest_framework.parsers import JSONParser
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-import json
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import parser_classes
 from django.views.decorators.csrf import csrf_exempt
 from library_api.models import Book, Author
 from library_api.serializers import BookSerializer, AuthorSerializer
@@ -26,16 +26,21 @@ def get_authors(request):
 
 
 @api_view(["POST", "GET"])
+@parser_classes([JSONParser])
 @csrf_exempt
 def add_book(request):
     if request.method == "POST":
-        payload = json.loads(request.body)
-        author = Author.objects.get(id=payload["author"])
         book = Book.objects.create(
-            title=payload["title"],
-            description=payload["description"],
+            title=request.data["title"],
+            description=request.data["description"],
         )
-        serializer = BookSerializer(book)
+        serializer = BookSerializer(data=request.data)
+        print(serializer)
+        if not serializer.is_valid():
+            return Response(
+                data= serializer.errors,
+                status=status.HTTP_404_NOT_FOUND
+            )
         return Response({'books': serializer.data}, status=status.HTTP_201_CREATED)
     if request.method == "GET":
         books = Book.objects.all()
@@ -43,15 +48,13 @@ def add_book(request):
         return Response({'books': serializer.data}, status=status.HTTP_200_OK)
 
 
-
 @api_view(["PUT"])
+@parser_classes([JSONParser])
 @csrf_exempt
 def update_book(request, book_id):
-    payload = json.loads(request.body)
     try:
         book_item = Book.objects.filter(id=book_id)
-        # returns 1 or 0
-        book_item.update(**payload)
+        book_item.update(**request.data)
         book = Book.objects.get(id=book_id)
         serializer = BookSerializer(book)
         return Response({'book': serializer.data}, status=status.HTTP_200_OK)
@@ -59,7 +62,6 @@ def update_book(request, book_id):
         return Response({'error': str(e)}, status=status.HTTP_404_NOT_FOUND)
     except Exception:
         return Response({'error': 'Something terrible went wrong'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
 
 
 @api_view(["DELETE"])
